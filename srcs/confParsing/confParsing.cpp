bfd2a8c5a3185ceb723af4d527272efcd2672b9f
spydersy <acbe1325@aol.com>
2022-02-05 01:01:06 +0100

    TODO: FIXI LIA SEGFAULT :')

--- srcs/confParsing/confParsing.cpp
+++ srcs/confParsing/confParsing.cpp
@@ -6,7 +6,7 @@
 /*   By: abelarif <abelarif@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 21:14:29 by abelarif          #+#    #+#             */
-/*   Updated: 2022/02/05 00:17:38 by abelarif         ###   ########.fr       */
+/*   Updated: 2022/02/05 01:00:21 by abelarif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,49 @@ bool    isEMptyLine(std::string LINE)
     if (LINE.length() == 0)
         return true;
     while (LINE[++i])
+    {
+        if (LINE[i] == '\t')
+            LINE[i] = ' ';
         if (LINE[i] != ' ' && LINE[i] != '\t')
             return false;
+    }
     return true;
 }
+void     semicolonConditions(std::vector<std::string> content, std::string LINE)
+{
+    // server KEYWORD CONDITIONS:
+    if (content[0].compare(KW_SERVER) == 0) {
+        if (content.size() == 1)
+            return ;
+        else if (content.size() == 2 && content[0].compare("{") == 0)
+            return ;
+        else if (content.size() > 2
+            || (content.size() == 2 && content[0].compare("{")))
+            errorStream(" in LINE : " + LINE, true, 1);
+    }
+    // curly bracket CONDITIONS:
+    if ((content[0].compare("{") == 0 || content[0].compare("}") == 0)
+        && content.size() == 1)
+        return ;
+    else if (content[0].compare("{") == 0
+        || content[0].compare("}") == 0)
+        errorStream(" in LINE : " + LINE, true, 1);
+    // other keywords:
+    if (content[content.size() - 1].compare(";")) {
+        errorStream(" in LINE : " + LINE, true, 1);
+    }
+}
 
-int    semicolonChecker(std::string LINE)
+void    semicolonChecker(std::string LINE)
 {
     std::vector<std::string>    content;
     std::string                 buffer;
 
     std::istringstream f(LINE);
-    while (getline(f, buffer, ';')) {
+    while (getline(f, buffer, ' ')) {
         content.push_back(buffer);
     }
+    semicolonConditions(content, LINE);
 }
 
 std::string    lineChecker(std::string LINE)
