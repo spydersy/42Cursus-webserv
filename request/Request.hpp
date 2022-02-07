3e87129c67fc2365a856b4ae027a12e45751f5a6
mohamedamine456 <aminelachheb456@gmail.com>
2022-02-07 19:21:46 +0100

    request class

---
+++ request/Request.hpp
@@ -0,0 +1,21 @@
+#ifndef REQUEST_CLASS_HPP
+#define REQUEST_CLASS_HPP
+
+#include <iostream>
+#include <vector>
+#include <utility>
+
+class Request {
+    private:
+        typedef std::pair<std::string, std::string>		string_pair;
+		std::string										method;
+		std::string										path;
+		std::string										query_string;
+		std::string										version;
+        std::vector<string_pair>						headers;
+		std::string										body;
+
+
+};
+
+#endif
